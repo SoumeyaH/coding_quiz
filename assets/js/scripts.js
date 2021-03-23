f4ccd3cb15feedbc39503646daee2aad4a91f8bc
Soumeya <soumeyahassan47@gmail.com>
2021-03-23 15:32:26 +0000

    wip added timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,38 @@
+const timerElement = document.getElementById("countDownTimer");
 const startButton = document.getElementById("initial-start-button");
-console.log(startButton);
+timerElement.textContent = "60 seconds left";
 
-const startQuiz = () => {
-  console.log("hello");
-};
+function timer() {
+  let timeLeft = 3;
+
+  const callback = function () {
+    if (timeLeft > 1) {
+      console.log(timeLeft);
+      timerElement.textContent = `${timeLeft} seconds left`;
+      timeLeft -= 1;
+    } else if (timeLeft === 1) {
+      console.log(timeLeft);
+      timerElement.textContent = `${timeLeft} second left`;
+      timeLeft -= 1;
+    } else if (timeLeft === 0) {
+      console.log(timeLeft);
+      timerElement.textContent = `times up`;
+      clearInterval(timeInterval);
+    }
+  };
+
+  // to do if time = 0 change text to times up
+
+  const timeInterval = setInterval(callback, 1000);
+}
+
+timer();
+
+const startQuiz = () => {};
 
 startButton.addEventListener("click", startQuiz);
+
+// start button is pressed
+// remove initial-items-container from main-container
+//put in first quiz question
+//start counter decrementing from 60 seconds
